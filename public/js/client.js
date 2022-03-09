ed2dd79dfe547754c306bbe5dac535bdfdf3c568
Suhaib Tariq <43761914+suhaib99@users.noreply.github.com>
2022-03-08 21:07:17 -0700

    chat functionality updated

--- public/js/client.js
+++ public/js/client.js
@@ -4,8 +4,11 @@ var messages = document.getElementById('messages');
 var form = document.getElementById('form');
 var input = document.getElementById('input');
 
-
-
+/*
+ Once chat is submitted, precvent default action of refreshing the page,
+ emit the message to server
+ clear out the message box field
+ */
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     if (input.value) {
@@ -13,6 +16,8 @@ form.addEventListener('submit', function (e) {
         input.value = '';
     }
 });
+
+
 
 socket.on('chat message', function (msg) {
     var item = document.createElement('li');
