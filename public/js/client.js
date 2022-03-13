9e83dcc01bc27053d82b95ea1ea4cb3fe465bcc7
Suhaib Tariq <43761914+suhaib99@users.noreply.github.com>
2022-03-12 23:31:54 -0700

    ask for name and colour

--- public/js/client.js
+++ public/js/client.js
@@ -1,31 +1,36 @@
 var socket = io();
 
-var messages = document.getElementById('messages');
-var form = document.getElementById('form');
-var messageInput = document.getElementById('message-input');
+var messages = document.getElementById("messages");
+var messageForm = document.getElementById("message-form");
+var messageInput = document.getElementById("message-input");
 
-var nickname = prompt('Please enter your nick name')
+var nameColourForm = document.getElementById("user-information-form");
+
+var nameEntered = document.getElementById("nickname-input");
+var colourEntered = document.getElementById("colour-input");
+
 
 /*
  Once chat is submitted, precvent default action of refreshing the page,
  emit the message to server
  clear out the message box field
  */
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    if (messageInput.value) {
-        socket.emit('send-message', messageInput.value);
-        messageInput.value = '';
-    }
+messageForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("hello");
+  if (messageInput.value) {
+    socket.emit("send-message", messageInput.value);
+    messageInput.value = "";
+  }
 });
 
 /*
 when a chat message is received, display it as a list item.
  */
 
-socket.on('send-message', function (msg) {
-    var item = document.createElement('li');
-    item.textContent = msg;
-    messages.prepend(item);
-    window.scrollTo(0, document.body.scrollHeight);
+socket.on("send-message", function (msg) {
+  var item = document.createElement("li");
+  item.textContent = msg;
+  messages.prepend(item);
+  window.scrollTo(0, document.body.scrollHeight);
 });
--- server.js
+++ server.js
@@ -6,15 +6,33 @@ const { Server } = require("socket.io")
 const io = new Server(server)
 var path = require('path')
 
+let bodyParser  = require( 'body-parser' )
 app.use(express.static(path.join(__dirname, 'public')));
+app.use( bodyParser({ extended: false }) )
 
 app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/landing-page.html')
+});
+
+let users = []
+
+app.get('/chat', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 });
+
+app.post( '/submit-user-info', ( req, res ) => {
+    res.redirect('/chat')
+    console.log(req.body)
+    users.push({nickname : req.body.nickname ,
+                colour : req.body.colour})
+});
+
+
 
 io.on('connection', (socket) => {
 
     socket.on('send-message', (msg) => {
+        
         io.emit('send-message', msg);
     });
 });
@@ -22,3 +40,4 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
+
